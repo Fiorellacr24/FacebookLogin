f60e10cd41cd8e4725b21d427e706d2ad1bfe262
Fiorellacr24 <fiorella.0624@gmail.com>
2017-06-13 15:51:33 -0500

    add resources

---
+++ assets/js/components/login.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Login = () => {
+    const login = $('<div class="login"></div>');
+    const title = $('<h1>Log into Laboratoria</h1>');
+    const email = $('<input type="email" class="username" placeholder="Correo electrónico"/>');
+    const password = $('<input type="password" class="password" placeholder="Contraseña"/>');
+    const loginButton = $('<button>Ingresa</button>');
+    const faceButton = $('<button>Ingresar con Facebook</button>');
+}
